77fcd81f7b5b71068f31ae29c9ebc63b846aaab3
Sana <sanamukhtar969@gmail.com>
2024-01-19 02:00:03 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,6 @@
+export default function App() {
+  return <>
+  <h1 className="text-3xl font-bold underline text-red-700">Hello world!</h1>
+  <div className="text-green-500">gireen</div>
+  </>
+}
---
+++ src/features/counter/counterSlice.ts
@@ -0,0 +1,50 @@
+
+import { createAppSlice } from "../../app/createAppSlice"
+import type { AppThunk } from "../../app/store"
+import { fetchCount } from "./counterAPI"
+
+export interface CounterSliceState {
+  value: number
+  status: "idle" | "loading" | "failed"
+}
+
+const initialState: CounterSliceState = {
+  value: 0,
+  status: "idle",
+}
+
+
+export const counterSlice = createAppSlice({
+  name: "counter",
+  
+  initialState,
+  
+  reducers: create => ({
+    increment: create.reducer(state => {
+      
+      state.value += 1
+    }),
+    decrement: create.reducer(state => {
+      state.value -= 1
+    }),
+    
+   
+   
+   
+  }),
+  
+  selectors: {
+    selectCount: counter => counter.value,
+    selectStatus: counter => counter.status,
+  },
+})
+
+
+export const { decrement, increment} =
+  counterSlice.actions
+
+
+export const { selectCount, selectStatus } = counterSlice.selectors
+
+
+
