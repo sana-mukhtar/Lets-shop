882cdae60e9afb72c61315cc8b73d54a355b76f6
Sana <sanamukhtar969@gmail.com>
2024-01-19 21:34:59 +0530

    made homepage

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
+import Navbar from "./features/navbar/Navbar";
 import ProductList from "./features/product-list/ProductList";
 
 
 export default function App() {
   return <>
+  <Navbar />
   <h1 className="text-3xl font-bold underline text-red-700">Hello world!</h1>
   <ProductList />
   </>
