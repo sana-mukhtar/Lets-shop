eb5c2d45d0f8e07a8cfe5037b0ddb1a29c4f6818
Sana <sanamukhtar969@gmail.com>
2024-01-27 03:16:38 +0530

    basic

--- lets-shop-frontend/src/App.tsx
+++ lets-shop-frontend/src/App.tsx
@@ -1,14 +1,23 @@
 import { BrowserRouter as Router , Routes , Route } from "react-router-dom"
+import { Suspense, lazy } from "react";
+import Loader from "./components/Loader";
+const Home = lazy(()=>import("./pages/Home"))
+const Cart = lazy(() => import("./pages/Cart"));
+const Search = lazy(() => import("./pages/Search"));
 
 
 const App = () => {
   return (
     <Router>
-      <Routes>
-        <Route></Route>
-      </Routes>
+      <Suspense fallback={<Loader />}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/search" element={<Search />} />
+        </Routes>
+      </Suspense>
     </Router>
-  )
+  );
 }
 
 export default App
