42123dc6c8145a468e25c902e3b1f3eec0e55251
Sana <sanamukhtar969@gmail.com>
2024-01-31 01:24:44 +0530

    Home

--- lets-shop-frontend/src/components/Header.tsx
+++ lets-shop-frontend/src/components/Header.tsx
@@ -1,9 +1,57 @@
-const Header = () => {
-  return (
-    <div>
-      Header
-    </div>
-  )
-}
+import { useState } from "react";
+import {
+  FaLongArrowAltRight,
+  FaSearch,
+  FaShoppingBag,
+  FaSignOutAlt,
+  FaUser,
+} from "react-icons/fa";
+import { Link } from "react-router-dom";
 
-export default Header
+const user = { _id: "asdf", role: "admin" };
+const Header = () => {
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+  return (
+    <nav className="flex justify-end items-stretch gap-14 flex-row h-20 bg-gray-100 p-5">
+      <Link to={"/"} className="font-bold">Home</Link>
+      <Link to={"/search"}>
+        <FaSearch />
+      </Link>
+      <Link to={"/cart"}>
+        <FaShoppingBag />
+      </Link>
+
+      <div className="">
+        {user?._id ? (
+          <>
+            <button onClick={() => setIsOpen((prev) => !prev)}>
+              <FaUser />
+            </button>
+
+            <dialog open={isOpen}>
+              <div>
+                {user?.role === "admin" && (
+                  <Link to={"/admin/dashboard"}>Admin</Link>
+                )}
+                <Link to={"/orders"}>Orders</Link>
+                <button>
+                  <Link to={"/logout"}>
+                    <FaSignOutAlt />
+                  </Link>
+                </button>
+              </div>
+            </dialog>
+          </>
+        ) : (
+          <Link to={"/login"}>
+            <button>
+              <FaLongArrowAltRight />
+            </button>
+          </Link>
+        )}
+      </div>
+    </nav>
+  );
+};
+
+export default Header;
--- lets-shop-frontend/src/pages/Home.tsx
+++ lets-shop-frontend/src/pages/Home.tsx
@@ -1,9 +1,29 @@
+import { Link } from "react-router-dom"
+
 const Home = () => {
   return (
-    <div>
-      Home
+    <div className="px-[5%] py-8  flex flex-col w-full">
+      <section className="bg-[url('/src/assets/camera.jpg')] bg-no-repeat bg-cover bg-center h-[20rem] w-full"></section>
+
+      <h1 className="text-2xl mt-12 mb-5 flex flex-row justify-between items-center">
+        <>LATEST PRODUCTS </>
+        <>
+          <Link to={"/search"} className="text-[1rem]">
+            {" "}
+            MORE{" "}
+          </Link>{" "}
+        </>
+      </h1>
+
+      <main className="flex flex-row  gap-4 w-full overflow-x-auto flex-wrap flex-1">
+        <div className="h-96 w-72 bg-green-700"></div>
+        <div className="h-96 w-72 bg-green-700"></div>
+        <div className="h-96 w-72 bg-green-700"></div>
+        <div className="h-96 w-72 bg-green-700"></div>
+     
+      </main>
     </div>
-  )
+  );
 }
 
 export default Home
