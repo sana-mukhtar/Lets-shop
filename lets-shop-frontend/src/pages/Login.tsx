010da48940e1a9fdeeac543eb175d49e05efacb7
Sana <sanamukhtar969@gmail.com>
2024-02-07 22:13:00 +0530

    Login complete

--- lets-shop-frontend/src/pages/Login.tsx
+++ lets-shop-frontend/src/pages/Login.tsx
@@ -1,48 +1,85 @@
-import { useState } from "react";
-import { FcGoogle } from "react-icons/fc";
 
-
-const Login = () => {
-    const [gender , setGender] = useState("");
-        const [date, setDate] = useState("");
-      
+export default function Login() {
   return (
-    <div className="login bg-white flex w-3/5 justify-center items-center h-[60vh] ">
-      <main className="flex flex-col ">
-        <h1 className="font-bold">LOGIN</h1>
-        <div className=" inline-flex p-2 ml-1 my-1">
-          <label> Gender </label>
-          <select
-            name=""
-            value={gender}
-            onChange={(e) => setGender(e.target.value)}
-            className=" border-gray-500 mx-2 border "
-          >
-            <option value="">Select Gender</option>
-            <option value="female">Female</option>
-            <option value="male">Male</option>
-          </select>
+    <>
+      <div className="flex min-h-full flex-1 flex-col justify-center   lg:px-0 bg-white h-[calc(100vh-5rem)]">
+        <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+          <h2 className="mt-10 text-center text-2xl font-bold leading-8 tracking-tight text-gray-900">
+            LOGIN
+          </h2>
         </div>
 
-        <div className="inline-flex  my-2 ">
-          <label htmlFor="">Date of birth</label>
-          <input
-            type="date"
-            value={date}
-            onChange={(e) => setDate(e.target.value)}
-            className="rounded-md p-0 mx-2 border border-gray-500"
-          />
-        </div>
+        <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+          <form className="space-y-6" action="#" method="POST">
+            <div>
+              <label
+                htmlFor="email"
+                className="block text-sm font-medium leading-6 text-gray-900"
+              >
+                Email address
+              </label>
+              <div className="mt-2">
+                <input
+                  id="email"
+                  name="email"
+                  type="email"
+                  autoComplete="email"
+                  required
+                  className="block w-full rounded-md border-0 py-1.5 px-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
 
-        <div className=" my-2">
-          <p className="">Sign In with Google</p>
-          <button className="inline-flex p-1 m-1 border rounded-md mt-3 border-gray-400">
-            <FcGoogle /> <span>Already Signed in Once</span>
-          </button>
+            <div>
+              <div className="flex items-center justify-between">
+                <label
+                  htmlFor="password"
+                  className="block text-sm font-medium leading-6 text-gray-900"
+                >
+                  Password
+                </label>
+                <div className="text-sm">
+                  <a
+                    href="#"
+                    className="font-semibold text-blue-800 hover:text-blue-500"
+                  >
+                    Forgot password?
+                  </a>
+                </div>
+              </div>
+              <div className="mt-2">
+                <input
+                  id="password"
+                  name="password"
+                  type="password"
+                  autoComplete="current-password"
+                  required
+                  className="block w-full rounded-md border-0 py-1.5 px-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-blue-700 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
+
+            <div>
+              <button
+                type="submit"
+                className="flex w-full justify-center rounded-md bg-blue-900 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-blue-700 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+              >
+                Sign in
+              </button>
+            </div>
+          </form>
+
+          <p className="mt-10 text-center text-sm text-gray-500">
+            Already signed in once? 
+            <a
+              href="#"
+              className="font-semibold leading-6 text-blue-800 hover:text-blue-500"
+            >
+              Sign in with Google
+            </a>
+          </p>
         </div>
-      </main>
-    </div>
+      </div>
+    </>
   );
 }
-
-export default Login
