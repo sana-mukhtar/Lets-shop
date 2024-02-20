b3b477c1e4f2e0fee85fa5eda8a0693c64252894
Sana <sanamukhtar969@gmail.com>
2024-02-21 02:37:58 +0530

    controller

--- lets-shop-backend/src/controllers/user.ts
+++ lets-shop-backend/src/controllers/user.ts
@@ -1,5 +1,17 @@
-import { NextFunction, Request, Response } from "express"
+import { NextFunction, Request, Response } from "express";
+import { User } from "../models/user.js";
 
-export const newUser(req:Request , res:Response , next:NextFunction) => {
-    
-}
+export const newUser = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const {} = req.body;
+    const user = await User.create({});
+    return res.status(200).json({
+      success: true,
+      message: `Welcome ${user.name}`,
+    });
+  } catch (error) {}
+};
