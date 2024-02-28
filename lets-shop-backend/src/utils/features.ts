78eb680d8c33c7cbf1d75d81a34400f7617a3df9
Sana <sanamukhtar969@gmail.com>
2024-02-28 21:40:08 +0530

    db connected

--- lets-shop-backend/src/utils/features.ts
+++ lets-shop-backend/src/utils/features.ts
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const connectDB=()=>{
     mongoose.connect("mongodb://localhost:27017", {
-      dbName: "Lets Shop",
+      dbName: "Lets_shop",
     })
     .then((c) => console.log(`db connected to ${c.connection.host} `))
     .catch((e) => console.log(e));
