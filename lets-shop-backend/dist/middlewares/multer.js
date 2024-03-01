a6fde9bb75b2d9abfaea20b241c920a4a6c27d81
Sana <sanamukhtar969@gmail.com>
2024-03-02 00:37:43 +0530

    multer

--- lets-shop-backend/dist/middlewares/multer.js
+++ lets-shop-backend/dist/middlewares/multer.js
@@ -1,1 +1,10 @@
-export {};
+import multer from "multer";
+const storage = multer.diskStorage({
+    destination(req, file, callback) {
+        callback(null, "uploads");
+    },
+    filename(req, file, callback) {
+        callback(null, file.originalname);
+    },
+});
+export const singleUpload = multer({ storage }).single("photo");
