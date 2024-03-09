de7544d7fef1d28898e6c73eefc23bb4c77a9989
Sana <sanamukhtar969@gmail.com>
2024-03-10 00:09:34 +0530

    order schema

--- lets-shop-backend/src/models/order.ts
+++ lets-shop-backend/src/models/order.ts
@@ -25,15 +25,50 @@ const schema = new mongoose.Schema(
       },
     },
 
-    user:{
-      type:String,
-      ref:"User",
-      required:true,
+    user: {
+      type: String,
+      ref: "User",
+      required: true,
     },
-    subtotal:{
-      type:Number,
-      required:true,
-    }
+    subtotal: {
+      type: Number,
+      required: true,
+    },
+    tax: {
+      type: Number,
+      required: true,
+    },
+    shippingCharges: {
+      type: Number,
+      required: true,
+    },
+    discount: {
+      type: Number,
+      required: true,
+    },
+    total: {
+      type: Number,
+      required: true,
+    },
+    status: {
+      type: String,
+      enum: ["Processing", "Shipped", "Delivered"],
+      default: "Processing",
+      required: true,
+    },
+
+    orderItems: [
+      {
+        name: String,
+        photo: String,
+        price: Number,
+        quantity: Number,
+        productId: {
+          type: mongoose.Types.ObjectId,
+          ref: "Product",
+        },
+      },
+    ],
   },
   {
     timestamps: true,
