54e613094636152e7e820d40806499914eff4f16
Sana <sanamukhtar969@gmail.com>
2024-03-15 03:31:16 +0530

    .

--- lets-shop-backend/src/controllers/payment.ts
+++ lets-shop-backend/src/controllers/payment.ts
@@ -1,5 +1,17 @@
+import { Request } from "express";
 import { TryCatch } from "../middlewares/error.js";
+import { Coupon } from "../models/coupon.js";
+import ErrorHandler from "../utils/utility-class.js";
 
-export const newCoupon = TryCatch(async(req,res,next)=>{
-    
-})
+export const newCoupon = TryCatch(async (req: Request, res, next) => {
+  const { coupon, amount } = req.body;
+
+  if (!coupon || !amount)
+    return next(new ErrorHandler("Enter Coupon code and discount amount", 400));
+
+  await Coupon.create({ code: coupon, amount });
+  return res.status(201).json({
+    success: true,
+    message: `Coupon ${coupon} Created Successfully`,
+  });
+});
