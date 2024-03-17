2e5cc024883823643e13b40be931a5fa88fe3f6b
Sana <sanamukhtar969@gmail.com>
2024-03-18 01:43:23 +0530

    Order Fulfillment Ratio(pie-chart)

--- lets-shop-backend/src/controllers/stats.ts
+++ lets-shop-backend/src/controllers/stats.ts
@@ -198,8 +198,33 @@ export const getDashboardStats = TryCatch(async (req, res, next) => {
   });
 });
 
-export const getPieCharts = TryCatch(async () => {
-  
+export const getPieCharts = TryCatch(async (req , res , next) => {
+  let charts;
+
+  if(myCache.has("admin-pie-charts")) charts = JSON.parse(myCache.get("admin-pie-charts") as string)
+
+  else{
+    const [processingOrder , shippedOrder , deliveredOrder] = await Promise.all([
+      Order.countDocuments({ status: "Processing" }),
+      Order.countDocuments({ status: "Shipped" }),
+      Order.countDocuments({ status: "Delivered" }),
+    ]);
+    const orderFulfillment = {
+      Processing : processingOrder,
+      shipped : shippedOrder,
+      delivered : deliveredOrder
+    } 
+ 
+    charts = {
+      orderFulfillment,
+    }
+
+    myCache.set("admin-pie-charts" , JSON.stringify(charts));
+  }
+   return res.status(200).json({
+     success: true,
+     charts,
+   });
 });
 
 export const getBarCharts = TryCatch(async () => {});
