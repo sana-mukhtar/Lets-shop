47b0ae6268830b62e8acdcd4134ac99e1a14ab4d
Sana <sanamukhtar969@gmail.com>
2024-03-29 02:43:13 +0530

    .

--- lets-shop-frontend/src/redux/api/userAPI.ts
+++ lets-shop-frontend/src/redux/api/userAPI.ts
@@ -4,18 +4,16 @@ import { MessageResponse } from "../../types/api-types";
 import { User } from "../../types/types";
 
 export const userAPI = createApi({
-  reducerPath: "userApi",
-  baseQuery: fetchBaseQuery({ baseUrl: `${server}/api/v1/user` }),
-  endpoints: (builder) => ({
-    login: builder.mutation<MessageResponse,User>({
-      query: (user) => ({
-        url: "new",
-        method: "POST",
-        body: user,
-      }),
-    }),
-    lol:builder.query({query:()=>""}),
+  reducerPath:"UserApi",
+  baseQuery:fetchBaseQuery({
+    baseUrl: `${server}/api/v1/user/`
   }),
-});
+  endpoints: (builder)=> ({
+    login:builder.mutation<MessageResponse,User>({query:(user)=>({
+      url:"new",
+      method:"POST",
+      body:user,
 
-export const { useLoginMutation } = userAPI
+    })})
+  })
+})
