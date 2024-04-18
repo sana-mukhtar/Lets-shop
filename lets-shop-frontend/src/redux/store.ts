2a9bf9f9b96c2430acc94a339788d842d08b75b1
Sana <sanamukhtar969@gmail.com>
2024-04-18 23:19:20 +0530

    .

--- lets-shop-frontend/src/redux/store.ts
+++ lets-shop-frontend/src/redux/store.ts
@@ -3,15 +3,15 @@ import { userAPI } from "./api/userAPI";
 import { setupListeners } from "@reduxjs/toolkit/query";
 import { userReducer } from "./reducer/userReducer";
 
-
 // export const server = import.meta.env.VITE_SERVER;
 
 export const store = configureStore({
   reducer: {
     [userAPI.reducerPath]: userAPI.reducer,
-    [userReducer.name]:userReducer.reducer,
+    [userReducer.name]: userReducer.reducer,
   },
-  middleware: (mid) => [...mid(), userAPI.middleware],
+  //middleware: (mid) => [...mid(), userAPI.middleware],
+  middleware: (mid) => mid().concat(userAPI.middleware),
 });
 
 setupListeners(store.dispatch);
